67779fe621b76b06e3f49fafae1e709678a46b4d
Abhinit Patil <abhineet0309@gmail.com>
2021-01-09 11:55:23 -0500

    Added database connection, schema and inital endpoints

---
+++ dbCards.js
@@ -0,0 +1,9 @@
+// file to outline database schema
+import mongoose from 'mongoose';
+
+const cardSchema = mongoose.Schema({
+    name: String,
+    imgUrl: String
+})
+
+export default mongoose.model('cards', CanvasRenderingContext2D)
---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://admine:rsGX3hP5QmGwbKcK@cluster0.qqykl.mongodb.net/tinderdb?retryWrites=true&w=majority`
+
+// Middleware
+
+// DB config 
+
+// mongoose command sbelow are to ensure consistency since mongoose is in continuous development
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send('HELLO WORLD!!!'))
+
+app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+// Listener
+app.listen(port, () => console.log(`listening in localhost: ${port}`));
+
